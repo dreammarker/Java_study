d75c6e545fd0317682fc631bf58805eb9c87d8d4
dreammarker <35982719+dreammarker@users.noreply.github.com>
2018-03-22 17:06:22 +0900

    java

--- 21장/src/fruit/FruitAndBox.java
+++ 21장/src/fruit/FruitAndBox.java
@@ -85,6 +85,7 @@ class Box3{ //중요함....제네릭 메소드와 와일드 카드 중 와일드
 	
 	static void outbox1(Box1<? extends Fruit> b){  //1.와일드카드? 메소드
 		Fruit o = b.get();    //상자에서 꺼내기만 가능
+		//사과
 		System.out.println(o);	
 		//b.set(new Fruit()); //상자에 넣기할 때 컴파일오류       
 	}
@@ -96,8 +97,9 @@ class Box3{ //중요함....제네릭 메소드와 와일드 카드 중 와일드
 	}
 		
 	static void inbox(Box1<? super Fruit> b, Fruit o){//★3.Fruit타입 또는 Fruit의 부모만 올수 있도록 제한
-		b.set(new Fruit());	//상자에 넣기만 가능
-//		Fruit o = b.get();  //상자에 꺼내기할때 자식=부모 오류...	자식=부모	
+		b.set(o);	//상자에 넣기만 가능
+//		o = b.get();  //상자에 꺼내기할때 자식=부모 오류...	자식=부모	
+	//자식
 	}	
 }
 
@@ -113,7 +115,7 @@ public class FruitAndBox {
 		
 		b.set("과일문자열"); //과일만을 담기위해 만든 상자에 문자열을 담을 수 있다.
 		//그래서 문자열이 담길 때 컴파일 오류를 ....해결방법:제네릭 클래스
-		
+		/***제네릭 클래스***/
 		Box1<Fruit> a = new Box1<Fruit>();  //객체생성시 타입이 결정됨.T를 Fruit로 결정 
 		Box1<Orange> o = new Box1<Orange>(); //오렌지 상자
 				
@@ -130,13 +132,13 @@ public class FruitAndBox {
 		
 		System.out.println(a2);
 		System.out.println(o2);
-		
+		/***제네릭 객체만든다음 *****/
 		Box2<String, Integer> b2 = new Box2<String, Integer>(); //생성시 
 		b2.set("사과", 3);//3은 기본타입->자동박싱되어 대입됨
 		System.out.println(b2);
 		
 		Generic g = new Generic();
-		g.<Fruit,Orange>show(a2, o2);
+		g./*<Fruit,Orange>*/show(a2, o2);//<Fruit,Orange>생략가능  이유:컴파일러가 알아서 유추.
 		//g.show(a2, o2);
 		
 		Box1<String> o3 = new Box1<String>();
